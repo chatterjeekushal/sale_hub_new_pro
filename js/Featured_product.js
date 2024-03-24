1a96b2535f5c23ef4a52316d90d30364464a43d0
chatterjeekushal <chatterjeekushal89@gmail.com>
2024-03-24 14:10:19 +0530

    add folder

---
+++ js/Featured_product.js
@@ -0,0 +1,95 @@
+
+
+let color_circles = document.querySelectorAll(".color_circle");
+
+let product_color = document.getElementById("product_color")
+
+let product_price = document.querySelector(".product_price")
+
+let product_image = document.querySelector("#product_image");
+
+//console.log(product_image);
+
+//console.log(color_circles);
+
+Array.from(color_circles).forEach((element, index) => {
+
+
+    element.addEventListener('click', function (e) {
+
+        let color_target = e.target.id
+
+        product_color.innerHTML = color_target;
+
+        if (color_target == "Olivegreen") {
+
+            product_price.innerHTML = "$120.00"
+            product_image.src = "./img/product_image/image0_olivegreen.jpg"
+        }
+
+        else if (color_target == "Black") {
+            product_price.innerHTML = "$140.00"
+
+            product_image.src = "./img/product_image/image1_black.jpg"
+        }
+
+        else if (color_target == "Peich") {
+            product_price.innerHTML = "$100.00"
+
+            product_image.src = "./img/product_image/image2_peich.jpg"
+
+        }
+
+
+        else if (color_target == "satewhite") {
+            product_price.innerHTML = "$160.00"
+
+            product_image.src = "./img/product_image/image3_white.jpg"
+        }
+
+
+        else if (color_target == "purple") {
+            product_price.innerHTML = "$150.00"
+
+            product_image.src = "./img/product_image/image4_purple.jpg"
+        }
+
+
+        else {
+
+            console.log("price not abalabel");
+        }
+
+
+    })
+
+});
+
+
+
+
+
+
+
+
+
+let product_panel_box = document.querySelectorAll(".product_panel_box");
+
+let product_size = document.querySelector("#product_size")
+
+console.log(product_size);
+
+console.log(product_panel_box);
+
+
+
+Array.from(product_panel_box).forEach((element, index) => {
+
+
+    element.addEventListener('click', function (e) {
+
+        product_size.innerHTML = e.target.id
+
+    })
+});
+
---
+++ js/nav.js
@@ -0,0 +1,79 @@
+
+
+let homelink=document.getElementById("home_list");
+
+let navhome=document.querySelector(".nav_home");
+
+let manindocument=document.querySelector("*");
+
+
+
+homelink.addEventListener("click",()=>{
+
+navhome.classList.toggle("d_none");
+
+})
+
+
+
+// manindocument.addEventListener("mouseenter",()=>{
+
+//     navhome.classList.add("d_none");
+    
+//     })
+
+
+
+let bloglink=document.getElementById("blog_list");
+
+let navblog=document.querySelector(".nav_blog")
+
+
+bloglink.addEventListener("click",()=>{
+
+    navblog.classList.toggle("d_none");
+    
+    })
+
+
+
+
+
+
+        // search area java script
+
+
+        let searchlink=document.querySelector(".search_list");
+
+        let searcharea=document.querySelector(".search_area");
+
+    searchlink.addEventListener("click",()=>{
+
+searcharea.classList.remove("search_area_top")
+
+    })
+
+    let close=document.querySelector(".close")
+
+    close.addEventListener('click',()=>{
+
+        searcharea.classList.add("search_area_top");
+        
+    })
+
+
+    let account=document.querySelector(".account");
+
+    let account_list=document.querySelector(".account_list");
+
+    account.addEventListener('click',function(){
+
+        account_list.classList.toggle("d_none")
+
+    })
+
+    // manindocument.addEventListener("mouseenter",()=>{
+
+    //     account_list.classList.add("d_none");
+        
+    //     })
