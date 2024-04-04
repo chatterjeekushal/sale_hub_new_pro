956fc4f46f30787926c0d6e39b869f4dae938059
chatterjeekushal <chatterjeekushal89@gmail.com>
2024-04-04 21:42:46 +0530

    add notify box

---
+++ js/shop_the_look.js
@@ -0,0 +1,16 @@
+// let shop_look_img = document.querySelectorAll(".shop_look_grid_item1 img");
+// let shop_look_btn = document.querySelectorAll(".shop_look_btn");
+
+// shop_look_img.forEach((img_element, img_index) => {
+//     img_element.addEventListener('click', function() {
+//         shop_look_btn.forEach((btn_element, btn_index) => {
+//             if (img_index === btn_index) {
+//                 btn_element.style.display = "flex";
+                
+//                 console.log(btn_element);
+//             } else {
+//                 btn_element.style.display = "none";
+//             }
+//         });
+//     });
+// });
